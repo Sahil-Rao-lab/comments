7d28800c79a4c70c2fbf4bb301a3ef2b593ca857
sahil <sahil@cueblocks.com>
2023-01-23 13:21:20 +0530

    adding try catch block

--- index.js
+++ index.js
@@ -45,47 +45,57 @@ app.post('/comment', async (req, res) => {
         ignoreHTTPSErrors: true,
     };
 
-    const browser = await puppeteer.launch(options);
-    let page = await browser.newPage();
-    await page.goto('https://www.instagram.com/');
-    page.setDefaultNavigationTimeout(0);
-    await page.waitForSelector('input[name=username]');
-    await page.waitForSelector('input[name=password]');
-    await page.type('input[name=username]', userName);
-    await page.type('input[name=password]', process.env.PASS);
-    await page.keyboard.press('Enter');
-    await page.waitForNavigation({ waitUntil: 'load' });
-    console.log('>>> USER LOGGED IN <<<<')
-    await page.goto(postUrl, { "waitUntil": "load" });
-    await page.waitForSelector('article > div > div > div > div > div > a');
-    let hrefs = await page.$$eval('a', as => as.map(a => a.href));
-    hrefs = hrefs.filter(a => a.includes('/p/'))
+    try {
 
-    for (let i = 0; i < hrefs.length; i++) {
+        const browser = await puppeteer.launch(options);
+        let page = await browser.newPage();
+        await page.goto('https://www.instagram.com/');
+        page.setDefaultNavigationTimeout(0);
+        await page.waitForSelector('input[name=username]');
+        await page.waitForSelector('input[name=password]');
+        await page.type('input[name=username]', userName);
+        await page.type('input[name=password]', process.env.PASS);
+        await page.keyboard.press('Enter');
+        await page.waitForNavigation({ waitUntil: 'load' });
+        console.log('>>> USER LOGGED IN <<<<')
+        await page.goto(postUrl, { "waitUntil": "load" });
+        await page.waitForSelector('article > div > div > div > div > div > a');
+        let hrefs = await page.$$eval('a', as => as.map(a => a.href));
+        hrefs = hrefs.filter(a => a.includes('/p/'))
 
-        page = await browser.newPage();
-        await page.goto(hrefs[i], { "waitUntil": 'domcontentloaded' });
-        try {
-            await page.waitForSelector('form > textarea');
-            await page.type(' form > textarea', 'Promote it on @social_.art.blog');
-            await page.keyboard.press('Tab');
-            await page.keyboard.press('Enter');
-            donePosts.push(hrefs[i]);
-            console.log('Enable Comment => ', hrefs[i])
-        } catch (error) {
-            falsePosts.push(hrefs[i]);
-            console.log('Disable Comment => ', hrefs[i])
-            continue;
+        for (let i = 0; i < hrefs.length; i++) {
+
+            page = await browser.newPage();
+            await page.goto(hrefs[i], { "waitUntil": 'domcontentloaded' });
+            try {
+                await page.waitForSelector('form > textarea');
+                await page.type(' form > textarea', 'Promote it on @social_.art.blog');
+                await page.keyboard.press('Tab');
+                await page.keyboard.press('Enter');
+                donePosts.push(hrefs[i]);
+                console.log('Enable Comment => ', hrefs[i])
+            } catch (error) {
+                falsePosts.push(hrefs[i]);
+                console.log('Disable Comment => ', hrefs[i])
+                continue;
+            }
+
         }
 
-    }
+        browser.close();
+        res.status(200).send({
+            success: true,
+            donePosts,
+            falsePosts,
+        })
 
-    browser.close();
-    res.status(200).send({
-        success: true,
-        donePosts,
-        falsePosts,
-    })
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({
+            success : false,
+            err : err
+        })
+    }
 
 })
 
